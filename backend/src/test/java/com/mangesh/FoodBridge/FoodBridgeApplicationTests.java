634343dadd7d538f14e2eb703964be2ce9b9a097
Mangesh Chavan <mangeshdchavan2130@gmail.com>
2026-04-05 23:50:06 +0530

    feat(backend): init Spring Boot 3 project with Maven

---
+++ backend/src/test/java/com/mangesh/FoodBridge/FoodBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangesh.FoodBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
